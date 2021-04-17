7f763b26e372d5151e289154836fd0b4189e22d2
Pedro Manuel <pmmarquez@xetid.cu>
2021-04-17 19:49:47 +0000

    [ADD] transfer method

--- models/payment.py
+++ models/payment.py
@@ -62,6 +62,23 @@ class PaymentAcquirerStripe(models.Model):
             return token.id
         else:
             return False
+    
+    def stripe_transfer(self, amount, currency, account_id):
+        # create transfer object
+        s2s_data_transfer = {
+            "amount": amount,
+            "currency": currency,
+            "destination": account_id
+        }
+        transfer = self._stripe_request('transfers', s2s_data_transfer)
+        # return transfer id
+        if transfer.get('id'):
+            self.env['bus.bus'].sendone(
+                self._cr.dbname + '_' + str(self.id),
+                {'type': 'stripe_transfer_vendor_notification', 'action':'created', "account_id":transfer.get('id')})
+            return transfer.get('id')
+        else:
+            return False
 
 class PaymentTransactionStripe(models.Model):
     _inherit = 'payment.transaction'
